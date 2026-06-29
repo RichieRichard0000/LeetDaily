e1593e9c1f05e1bcbba37520393557522ae86a6f
Devansh Baranwal <83960769+RichieRichard0000@users.noreply.github.com>
2026-06-29 17:04:25 +0530

    Time: 180 ms (31.39%) | Memory: 106.7 MB (19.57%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,18 +1,53 @@
 class Solution {
 public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
-        for (string& s : strs) {
-            int zeros = count(s.begin(), s.end(), '0');
-            int ones = s.size() - zeros;
-            for (int i = m; i >= zeros; --i) {
-                for (int j = n; j >= ones; --j) {
-                    dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1);
+    pair<int,int> help(string s)
+    {
+        int z=0,o=0;
+
+        for(char c:s)
+        {
+            if(c=='0') z++;
+            else o++;
+        }
+
+        return {z,o};
+    }
+
+    int findMaxForm(vector<string>& strs, int m, int n) {
+
+        int sz=strs.size();
+
+        vector<vector<vector<int>>> dp(
+            sz+1,
+            vector<vector<int>>(m+1, vector<int>(n+1,0))
+        );
+
+        for(int ind=sz-1;ind>=0;ind--)
+        {
+            auto cnt=help(strs[ind]);
+
+            int z=cnt.first;
+            int o=cnt.second;
+
+            for(int zero=0;zero<=m;zero++)
+            {
+                for(int one=0;one<=n;one++)
+                {
+                    int skip=dp[ind+1][zero][one];
+
+                    int take=0;
+
+                    if(zero>=z && one>=o)
+                    {
+                        take=1+dp[ind+1][zero-z][one-o];
+                    }
+
+                    dp[ind][zero][one]=max(take,skip);
                 }
             }
         }
 
-        return dp[m][n];
+        return dp[0][m][n];
     }
 };
